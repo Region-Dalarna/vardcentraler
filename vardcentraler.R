76c467307a6c42f7102b5a914aefa1282101446d
hen-ald <henrik.alden@regiondalarna.se>
2023-10-05 11:51:00 +0200

    förenklad kod och data

--- vardcentraler.R
+++ vardcentraler.R
@@ -4,18 +4,10 @@ library(mapview)
 library(dplyr)
 library(tidyverse)
 
-# Utbudspunkter_kopia <- read_excel("Utbudspunkter_kopia.xlsx")
-# head(Utbudspunkter_kopia)
-# vardcentraler <- st_as_sf(Utbudspunkter_kopia,
-#                           coords = c("Longitude", "Latitude"),
-#                           crs = 4326)
-#SWEREF istället
+#SWEREF 
 vardcentraler <- st_as_sf(Utbudspunkter_kopia,
                           coords = c("Sweref99Y", "Sweref99X"),
                           crs = 3006)
-
-
-mapview(vardcentraler, label = "Populärnamn")
 
 #hämta Regso från G:\Samhällsanalys\GIS\grundkartor\regso
 
@@ -34,33 +26,18 @@ regso_dalarna_id <- regso_dalarna %>%
 vardcentral_inter <- vardcentraler %>% 
   mutate(regso = as.integer(st_intersects(., regso_dalarna_id))) 
 
-# Filtrerar på FGhKlass
+# Filtrerar på FGhKlass, kanske mer lämpligt att filtrera på HSA_namnVC?
 vardcentraler_endast <- vardcentral_inter  %>% 
   filter(FghKlass == "Vårdcentral")
 
-#Kommun gränser från regso
-
+#Kommungränser från regso
 kommun <- regso_dalarna %>% 
   group_by(kommun, kommunnamn) %>% 
   summarize(geom = st_union(geom)) %>% 
   ungroup()
 
-#från chatgpt
-kommun <- regso_dalarna %>%
-  group_by(kommun, kommunnamn) %>% 
-  ungroup()
-
-mapview(kommun, label = "kommunnamn", alpha.regions = 0)
-
-mapview(vardcentraler_endast, label = "Populärnamn")+
-  mapview(regso_dalarna_id, zcol = "kommun", legend = FALSE, hide = TRUE, alpha.regions = 0.5)+
-  mapview(kommun, label = "kommunnamn", alpha.regions = 0)
-
-library(sf)
-
-# Assuming you have loaded your polygon and point layers into regso_dalarna_id and vardcentraler_endast
-
-# Perform a spatial join to associate each point with its containing polygon
+# Chatgpt briljerar med följande som lägger till en kolumn i regso respektive kommun med antal vårdcentraler
+#Perform a spatial join to associate each point with its containing polygon
 joined_data_regso <- st_join(vardcentraler_endast, regso_dalarna_id)
 
 # Group by regso and count the vardcentral within each polygon
